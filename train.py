c9b4a8e14c3cac2bd5ba9536caeda410053ec0a0
Sherzod <sfreedom9393@gmail.com>
2024-08-19 15:19:49 +0500

    train.py is updated

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from ruamel.yaml import YAML
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def train_and_evaluate(train_json_path, test_json_path, model_name,num_epochs, model_save_path, checkpoint):
+def train_and_evaluate(train_json_path, test_json_path, model_name, num_epochs, model_save_path, checkpoint):
     # --- Config Information ---#
     config_path = './configs/config.yaml'
     if not os.path.exists(config_path):
@@ -32,8 +32,8 @@ def train_and_evaluate(train_json_path, test_json_path, model_name,num_epochs, m
         new_opt['lr'] = 0.001
         # Transfer Learning
         logging.info(f"Loading pre-trained model: {model_name} for transfer learning...")
-        asr_model = nemo_asr.models.EncDecCTCModel.from_pretrained(model_name=model_name)
-        
+        asr_model = nemo_asr.models.EncDecCTCModel.restore_from(restore_path=model_name)
+
         logging.info(f"Changing vocabulary from: {asr_model.decoder.vocabulary}")
         asr_model.change_vocabulary(new_vocabulary=[' ', 'a', 'b','d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z', "1", "2", "3", "4", "5", "'"])
         logging.info(f"New vocabulary: {asr_model.decoder.vocabulary}")
@@ -76,7 +76,7 @@ def train_and_evaluate(train_json_path, test_json_path, model_name,num_epochs, m
         for test_batch in asr_model.test_dataloader():
             test_batch = [x.cuda() for x in test_batch]
             targets = test_batch[2]
-            targets_lengths = test_batch[3]        
+            targets_lengths = test_batch[3]
             log_probs, encoded_len, greedy_predictions = asr_model(input_signal=test_batch[0], input_signal_length=test_batch[1])
             asr_model._wer.update(greedy_predictions, targets, targets_lengths)
             _, wer_num, wer_denom = asr_model._wer.compute()
@@ -87,14 +87,15 @@ def train_and_evaluate(train_json_path, test_json_path, model_name,num_epochs, m
 
         logging.info(f"WER = {sum(wer_nums)/sum(wer_denoms)}")
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train and evaluate an ASR model using NeMo.")
     parser.add_argument("--train_json_path", type=str,  default="./train.json", help="Path to the training JSON file.")
     parser.add_argument("--test_json_path", type=str, default = "./test.json", help="Path to the testing JSON file.")
-    parser.add_argument("--model_name", type=str, default=None, help="Name of the pre-trained model for transfer learning. If not provided, train from scratch.")
+    parser.add_argument("--model_name", type=str, default='saved_model/quartznet15x5.pt', help="Name of the pre-trained model for transfer learning. If not provided, train from scratch.")
     parser.add_argument("--model_save_path", type=str, default="./asr_model.nemo", help="Path to save the trained ASR model.")
     parser.add_argument("--checkpoint", action="store_true", help="Whether to evaluate the model or not.")
-    parser.add_argument("--num_epochs", type=int, default=180, help="Number of epochs to train the model.")
+    parser.add_argument("--num_epochs", type=int, default=10, help="Number of epochs to train the model.")
 
     args = parser.parse_args()
     train_and_evaluate(args.train_json_path, args.test_json_path, args.model_name, args.num_epochs, args.model_save_path, args.checkpoint)
